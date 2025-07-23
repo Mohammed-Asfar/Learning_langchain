edb3a0d807938b15e78d14742664d68de34a6dfb
Mohammed Asfar A <asfarmohammed49@gmail.com>
2025-07-23 18:22:24 +0530

    chat_model_conversation_with_user.py done

---
+++ langchainByHarish/chat_model/chat_model_conversation_with_user.py
@@ -0,0 +1,20 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat_history = [
+    SystemMessage("Your name is Kana. A ai assistant of Mohammed Asfar."),
+]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+while True:
+    n = input(">>")
+    if n == "exit":
+        break
+    chat_history.append(HumanMessage(n))
+    result = llm.invoke(chat_history)
+    chat_history.append(AIMessage(result.content))
+    print(result.content)
