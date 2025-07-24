8899f14411c01b7d93e51f27fa315c881edeb11f
Mohammed Asfar A <asfarmohammed49@gmail.com>
2025-07-24 18:25:43 +0530

    chains_inner_workings.py done

--- langchainByHarish/chains/chains_inner_workings.py
+++ langchainByHarish/chains/chains_inner_workings.py
@@ -2,6 +2,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableLambda, RunnableSequence
 
 load_dotenv()
 
@@ -15,8 +16,14 @@ prompt_template = ChatPromptTemplate.from_messages(
     ]
 )
 
-chain = prompt_template | llm | StrOutputParser()
+format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
+invoke_llm = RunnableLambda(lambda x: llm.invoke(x.to_messages()))
+parse_output = RunnableLambda(lambda x: x.content)
 
-result = chain.invoke({"animal": "cat", "fact_count": 2})
 
-print(result)
+chain = RunnableSequence(first=format_prompt, middle=[invoke_llm], last=parse_output)
+
+
+res = chain.invoke({"animal": "cat", "fact_count": 2})
+
+print(res)
